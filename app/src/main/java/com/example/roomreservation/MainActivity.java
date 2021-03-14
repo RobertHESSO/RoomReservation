81428072220cb67eb0aa61b08a88ddcfc0b2f829
RobertHESSO <robert.lacic@students.hevs.ch>
2021-03-14 16:06:13 +0100

    RoomReservation

---
+++ app/src/main/java/com/example/roomreservation/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.roomreservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private TextView register;
+    private Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        register = (TextView) findViewById(R.id.textView3);
+        register.setOnClickListener(this);
+
+        login = (Button) findViewById(R.id.button4);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.textView3:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+            case R.id.button4:
+                startActivity(new Intent(this,Search.class));
+
+        }
+    }
+}
