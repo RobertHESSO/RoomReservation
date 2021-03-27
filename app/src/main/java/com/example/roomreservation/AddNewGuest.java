dd5c435b9d18fb04a0eabc305899b5a9b5806c38
Raffael <raffael.imboden@students.hevs.ch>
2021-03-27 13:05:15 +0100

    versucht DatePickerDialog für checkIn,checkout zu erstellen

--- app/src/main/java/com/example/roomreservation/AddNewGuest.java
+++ app/src/main/java/com/example/roomreservation/AddNewGuest.java
@@ -2,19 +2,30 @@ package com.example.roomreservation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class AddNewGuest extends AppCompatActivity implements View.OnClickListener {
+import java.util.Calendar;
 
+public class AddNewGuest extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener  {
+
+    private DatePickerDialog datePickerDialog;
+    private TextView checkIn;
+    private TextView checkOut;
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_guest);
+        checkIn = findViewById(R.id.CheckIn);
 
         button = (Button) findViewById(R.id.SubmitButton);
         button.setOnClickListener(this);
@@ -28,6 +39,26 @@ public class AddNewGuest extends AppCompatActivity implements View.OnClickListen
             case R.id.SubmitButton:
                 startActivity(new Intent(this, MainMenu.class));
                 break;
+            case R.id.CheckIn:
+                showDatePickerDialog();
+                break;
+            case R.id.CheckOut:
+                showDatePickerDialog();
+                break;
         }
+    }
+
+    public void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = "month/day/year: " + month + "/" + dayOfMonth + "/" + year;
+        checkIn.setText(date);
     }
 }
