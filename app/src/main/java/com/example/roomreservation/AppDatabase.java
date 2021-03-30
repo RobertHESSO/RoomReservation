d0e9011d5bc021bd99de85e523b6c502eb1b9efd
Raffael <raffael.imboden@students.hevs.ch>
2021-03-30 15:07:30 +0200

    AppDatabase gelöscht

---
+++ app/src/main/java/com/example/roomreservation/AppDatabase.java
@@ -0,0 +1,67 @@
+package com.example.roomreservation;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.Executors;
+
+import dao.ReservationDao;
+import dao.RoomDao;
+import entity.ReservationEntity;
+import entity.RoomEntity;
+
+public class AppDatabase {
+
+    @Database(entities = {RoomEntity.class, ReservationEntity.class}, version = 1)
+    public abstract class AppDatabase extends RoomDatabase {
+
+        private static final String TAG = "AppDatabase";
+
+        private static com.example.roomreservation.AppDatabase instance;
+
+        private static final String DATABASE_NAME = "RoomReservation-Database";
+
+        public abstract ReservationDao reservationDao();
+        public abstract RoomDao roomDao();
+
+
+        private final MutableLiveData<Boolean> isDatabaseCreated = new MutableLiveData<>();
+
+        public static com.example.roomreservation.AppDatabase getInstance(final Context context) {
+            if (instance == null) {
+                synchronized (com.example.roomreservation.AppDatabase.class) {
+                    if (instance == null) {
+                        instance = buildDatabase(context.getApplicationContext());
+                        instance.updateDatabaseCreated(context.getApplicationContext());
+                    }
+                }
+            }
+            return instance;
+        }
+
+
+        private static com.example.roomreservation.AppDatabase buildDatabase(final Context appContext) {
+            Log.i(TAG, "Database will be initialized.");
+            return Room.databaseBuilder(appContext, com.example.roomreservation.AppDatabase.class, DATABASE_NAME)
+                    .addCallback(onCreate(db) {
+                super.onCreate(db);
+                Executors.newSingleThreadExecutor().execute(() -> {
+                    com.example.roomreservation.AppDatabase database = com.example.roomreservation.AppDatabase.getInstance(appContext);
+                    DatabaseInitializer.populateDatabase(database);
+                    database.setDatabaseCreated();
+                });
+            }).build();
+        }
+
+        private void updateDatabaseCreated(final Context context) {
+            if (context.getDatabasePath(DATABASE_NAME).exists()) {
+                Log.i(TAG, "Database initialized.");
+                setDatabaseCreated();
+            }
+        }
+}
